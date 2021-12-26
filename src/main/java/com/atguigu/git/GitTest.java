88fce34215825d631ec6b3249b4ae486df0dd6e2
gouyuan <gouyuan0201@163.com>
2021-12-26 17:56:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author VX:Feb-01-Aqr
+ * @create 2021/12/26 - 17:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
